ce87c768c2249b0be1b9643fe7ebfa6140f7587c
owenbhp <backhandpower22@gmail.com>
2016-09-20 11:44:31 +0800

    Add files via upload

---
+++ OwenOrmSample/OwenOrmSample/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace OwenOrmSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.firstName = new System.Windows.Forms.TextBox();
+            this.middleName = new System.Windows.Forms.TextBox();
+            this.lastName = new System.Windows.Forms.TextBox();
+            this.birthday = new System.Windows.Forms.DateTimePicker();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deletebutton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.personId = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Middle Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Birthday";
+            // 
+            // firstName
+            // 
+            this.firstName.Location = new System.Drawing.Point(102, 45);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(212, 20);
+            this.firstName.TabIndex = 4;
+            // 
+            // middleName
+            // 
+            this.middleName.Location = new System.Drawing.Point(102, 78);
+            this.middleName.Name = "middleName";
+            this.middleName.Size = new System.Drawing.Size(212, 20);
+            this.middleName.TabIndex = 5;
+            // 
+            // lastName
+            // 
+            this.lastName.Location = new System.Drawing.Point(102, 113);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(212, 20);
+            this.lastName.TabIndex = 6;
+            // 
+            // birthday
+            // 
+            this.birthday.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.birthday.Location = new System.Drawing.Point(102, 145);
+            this.birthday.Name = "birthday";
+            this.birthday.Size = new System.Drawing.Size(118, 20);
+            this.birthday.TabIndex = 7;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(7, 205);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.Size = new System.Drawing.Size(560, 186);
+            this.dataGrid.TabIndex = 8;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(363, 42);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 9;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(363, 75);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 10;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // deletebutton
+            // 
+            this.deletebutton.Location = new System.Drawing.Point(363, 110);
+            this.deletebutton.Name = "deletebutton";
+            this.deletebutton.Size = new System.Drawing.Size(75, 23);
+            this.deletebutton.TabIndex = 11;
+            this.deletebutton.Text = "Delete";
+            this.deletebutton.UseVisualStyleBackColor = true;
+            this.deletebutton.Click += new System.EventHandler(this.deletebutton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "PersonId";
+            // 
+            // personId
+            // 
+            this.personId.AutoSize = true;
+            this.personId.Location = new System.Drawing.Point(102, 16);
+            this.personId.Name = "personId";
+            this.personId.Size = new System.Drawing.Size(0, 13);
+            this.personId.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 396);
+            this.Controls.Add(this.personId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.deletebutton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.birthday);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.middleName);
+            this.Controls.Add(this.firstName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Owen.Orm Simple Example";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox firstName;
+        private System.Windows.Forms.TextBox middleName;
+        private System.Windows.Forms.TextBox lastName;
+        private System.Windows.Forms.DateTimePicker birthday;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deletebutton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label personId;
+    }
+}
+
---
+++ OwenOrmSample/OwenOrmSample/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+using Owen.Orm;
+using System.Data.SQLite;
+
+namespace OwenOrmSample
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"data source = Test.db; version = 3";
+        Table<SQLiteConnection, Person> personTable;
+
+        public Form1()
+        {
+            prepareDatabaseFile();
+            InitializeComponent();
+            personTable = new Table<SQLiteConnection, Person>(connectionString);
+            refreshData();
+        }
+
+        private void refreshData()
+        {
+            dataGrid.DataSource = personTable.ToList(); //read all data from database
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            var p = new Person
+            {
+                FirstName = firstName.Text,
+                MiddleName = middleName.Text,
+                LastName = lastName.Text,
+                Birthday = birthday.Value
+            };
+            personTable.Insert(p);
+            refreshData();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = (int)dataGrid.CurrentRow.Cells[0].Value;
+                var p = new Person
+                {
+                    PersonId = id,
+                    FirstName = firstName.Text,
+                    MiddleName = middleName.Text,
+                    LastName = lastName.Text,
+                    Birthday = birthday.Value
+                };
+                personTable.Update(p);
+                refreshData();
+            }
+            catch { }
+        }
+
+        private void deletebutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = (int)dataGrid.CurrentRow.Cells[0].Value;
+                personTable.Delete(id);
+                refreshData();
+            }
+            catch { }
+        }
+
+        void prepareDatabaseFile()
+        {
+            var q = new Query<SQLiteConnection, Blank>(connectionString, null);
+            //create the table in our database
+            q.ExecuteNonQuery(@"CREATE TABLE IF NOT EXISTS Person(PersonId INTEGER PRIMARY KEY AUTOINCREMENT, FirstName VARCHAR(20), MiddleName VARCHAR(20), LastName VARCHAR(20), Birthday  DATETIME);");
+        }
+
+        //i just need this to execute a query
+        class Blank : DomainObject { }
+    }
+
+    public class Person : DomainObject
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? Birthday { get; set; }
+    }
+}
